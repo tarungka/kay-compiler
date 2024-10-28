3561da3d9ac95078c5ca473a72101248e65c1a7a
Tarun Gopalkrishna A <tarungopalkrishna@gmail.com>
2024-10-28 23:11:06 +0530

    format code

--- main.py
+++ main.py
@@ -6,31 +6,21 @@ from tokenizer import Tokenizer
 from parser import Parser, print_ast
 from kast import ProgramNode
 
-code = """
-val x = 3
-
-if x > 3 {
-    val b = 10
-    somefn(b)
-    print(a)
-}
-"""
-
 regex_tokens = [
-            ("IF", r'if'),
-            ("VAL", r'val'),
-            ("COND", r'==|>=|<=|>|<|!='),
-            ("EQ", r'='),
-            ("LBRACE", r'\{'),
-            ("RBRACE", r'\}'),
-            ("LPARENS", r'\('),
-            ("RPARENS", r'\)'),
-            ("NUMBER", r'[0-9]+'),
-            ("NAME", r'[a-z]+'),
-            ("WHITESPACE", r'[ ]+'),
-            ("WHITESPACE_TAB", r'[\t]+'), # assuming tab is 4 spaces long
-            ("NEWLINE", r'\n'),
-        ]
+    ("IF", r'if'),
+    ("VAL", r'val'),
+    ("COND", r'==|>=|<=|>|<|!='),
+    ("EQ", r'='),
+    ("LBRACE", r'\{'),
+    ("RBRACE", r'\}'),
+    ("LPARENS", r'\('),
+    ("RPARENS", r'\)'),
+    ("NUMBER", r'[0-9]+'),
+    ("NAME", r'[a-z]+'),
+    ("WHITESPACE", r'[ ]+'),
+    ("WHITESPACE_TAB", r'[\t]+'), # assuming tab is 4 spaces long
+    ("NEWLINE", r'\n'),
+]
 
 
 
